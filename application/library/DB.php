cd7dc8eee51f65d1959b31f06443724f19a9a719
liuchao <php_liuchao@163.com>
2017-09-24 22:44:54 +0800

    DB类

--- application/library/DB.php
+++ application/library/DB.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * DB 操作类，提供类似laravel中DB facade的用法
+ * DB 操作类
  *
  * @Author   liuchao
  * Class DB
@@ -9,49 +9,17 @@
 class DB {
 
     /**
-     * @var array 已经添加的数据库连接
-     */
-    private static $connections = [];
-
-    /**
-     * 获取一个指定连接的Capsule实例
-     *
-     * @Author   liuchao
-     *
-     * @param string $name
-     *
-     * @return mixed
-     * @throws \Exception
-     */
-    public static function connection($name = 'default'){
-
-        $capsule = Yaf_Registry::get('Capsule');
-
-        if(in_array($name, self::$connections)){
-            return $capsule->connection($name);
-        }
-
-        if($config = config("database.$name")){
-            $connections[] = $name;
-            $capsule->addConnection($config, $name);
-            return $capsule->connection($name);
-        }
-
-        throw new Exception('连接不存在');
-    }
-
-    /**
-     * 代理静态方法调用
+     * 静态方法调用代理
      *
      * @Author   liuchao
      *
      * @param $method
-     * @param $args
+     * @param $parameters
      *
      * @return mixed
      */
-    public static function __callStatic($method, $args){
-
-        return call_user_func_array("Illuminate\Database\Capsule\Manager::$method", $args);
+    public static function __callStatic($method, $parameters){
+        return container('db')->$method(...$parameters);
     }
+
 }
