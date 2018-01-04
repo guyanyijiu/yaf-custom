067fd6dcff77349e1b83865bb75ada05db4eabb4
bing <user.email>
2018-01-04 18:18:14 +0800

    contrib

---
+++ contrib/moxie/MailCard/FindInfo.php
@@ -0,0 +1,30 @@
+<?php
+namespace Moxie\MailCard;
+use Moxie\MoxieBase;
+use Moxie\Https;
+
+/**
+ * 邮箱信用卡获取信息
+ */
+class FindInfo extends MoxieBase{
+
+	const FINDINFO='email/v2/alldata';
+	/**
+	 * 获取数据
+	 * @param  [type] $task_id [description]
+	 * @return [type]          [description]
+	 */
+	public function getData($data){
+		$url=self::BASE_URL.self::FINDINFO;
+		$res=Https::get($url,$data);
+		return $res;
+	}
+
+
+}
+
+
+
+
+
+?>
