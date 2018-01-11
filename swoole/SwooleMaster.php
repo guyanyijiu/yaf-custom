a4db2359cff7ae887d8c1813fc4ace95810c0eae
liuchao <php_liuchao@163.com>
2018-01-11 15:34:40 +0800

    多进程管理类

--- swoole/SwooleMaster.php
+++ swoole/SwooleMaster.php
@@ -2,6 +2,16 @@
 
 require __DIR__ . '/../cli.php';
 
+/**
+ * 多进程任务处理类
+ * 适用于各进程可以独立处理的任务，子进程之间互不通信互不影响
+ * 主进程会在子进程退出之后重新启动新的子进程
+ * 子进程会在主进程退出之后处理完当前任务之后自动退出
+ *
+ * Class SwooleMaster
+ *
+ * @author  liuchao
+ */
 class SwooleMaster {
 
     /**
@@ -30,15 +40,20 @@ class SwooleMaster {
      *
      * @var int
      */
-    protected $max_reboot = 10;
+    protected $max_reboot = 100;
 
     /**
      * 每个子进程处理的最大任务数，处理完之后自动退出，可以防止内存泄漏
      *
      * @var int
      */
-    protected $max_worker_task = 100;
+    protected $max_worker_task = 1000000;
 
+    /**
+     * 进程名字，macOS 不支持设置进程名
+     *
+     * @var string
+     */
     protected $proccess_name = 'default';
 
     /**
@@ -46,13 +61,15 @@ class SwooleMaster {
      */
     public function __construct() {
         try {
+            \Log::info('测试');
             swoole_process::daemon();
-            swoole_set_process_name('php beanstalkd consumer ' . $this->proccess_name . ' : master');
+            swoole_set_process_name('php ' . $this->proccess_name . ' : master');
             $this->master_pid = posix_getpid();
+
             $this->run();
             $this->processWait();
         } catch (\Exception $e) {
-            Log::error('swoole 进程异常:' . $e->getMessage());
+            \Log::error($this->proccess_name . '进程启动异常:' . $e->getMessage());
             exit;
         }
     }
@@ -70,7 +87,7 @@ class SwooleMaster {
     }
 
     /**
-     * 启动子进程，创建任务
+     * 启动子进程并开始执行任务
      *
      * @param $index
      *
@@ -80,21 +97,24 @@ class SwooleMaster {
      */
     public function startWorker($index) {
         $process = new swoole_process(function (swoole_process $worker) use ($index) {
-            swoole_set_process_name('php beanstalkd consumer ' . $this->proccess_name . ' : worker-' . $index);
-            for ($j = 0; $j < $this->max_worker_task; $j++) {
+            swoole_set_process_name('php ' . $this->proccess_name . ' : worker-' . $index);
+            $j = $this->max_worker_task;
+            while ($j > 0) {
                 $this->checkMaster($worker);
+
                 $res = false;
-                try{
+                try {
                     $res = $this->task();
-                }catch (\Exception $e){
-                    Log::error('任务执行异常:' . $e->getMessage());
+                } catch (\Throwable $e) {
+                    \Log::error('任务执行异常:' . $e->getMessage());
                 }
                 if ( !$res) {
-                    $j--;
                     sleep(3);
                     continue;
                 }
+                $j--;
             }
+
         }, false, false);
 
         $pid = $process->start();
@@ -112,7 +132,7 @@ class SwooleMaster {
      */
     public function checkMaster(&$worker) {
         if ( !swoole_process::kill($this->master_pid, 0)) {
-            Log::error("Master process exited, I [{$worker->pid}] also quit");
+            \Log::error($this->proccess_name . " 父进程退出, 子进程 [{$worker->pid}] 也退出");
             $worker->exit();
         }
     }
@@ -122,30 +142,29 @@ class SwooleMaster {
      *
      * @param $ret
      *
-     * @throws Exception
-     *
      * @author  liuchao
      */
     public function rebootWorker($ret) {
         $pid = $ret['pid'];
         $code = $ret['code'];
-        if($code !== 0){
-            $this->max_reboot --;
+        if ($code !== 0) {
+            $this->max_reboot--;
         }
-        Log::info('退出码:' . $code);
-        Log::info('max_reboot:' . $this->max_reboot);
-        if($this->max_reboot < 0 ){
-            Log::error('已达到异常退出重启最大次数');
+        \Log::info('子进程退出: code=' . $code);
+
+        if ($this->max_reboot < 0) {
+            \Log::error('已达到子进程异常退出重启最大次数');
             exit;
         }
         $index = array_search($pid, $this->workers);
         if ($index !== false) {
             $index = intval($index);
             $new_pid = $this->startWorker($index);
-            Log::info("rebootWorker: {$index}={$new_pid} Done\n");
+            \Log::info("重启子进程: {$index}={$new_pid} 完成");
+
             return;
         }
-        Log::error('rebootWorker Error: no pid');
+        \Log::error('重启子进程错误: no pid');
     }
 
     /**
@@ -169,6 +188,8 @@ class SwooleMaster {
 
     /**
      * 子进程的任务
+     * 如果该任务返回 true 则认为任务执行成功一次，并立即进行下一次执行
+     * 如果返回 false 则认为任务执行失败一次， sleep(3) 之后再进行下一次执行
      *
      * @return bool
      *
